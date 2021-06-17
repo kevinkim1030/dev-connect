5b930cfda91f1877fdb58f66b63c0c8e8d11e509
kevinkim1030 <kevin.kim1030@gmail.com>
2021-06-17 14:48:02 -0400

    post item component

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,13 @@ import { combineReducers } from 'redux';
 import alert from './alert';
 import auth from './auth';
 import profile from './profile';
+import post from './post';
 
 export default combineReducers({
+  // to add anything to app
+    // create new reducer/actions file/component
   alert,
   auth,
-  profile
+  profile,
+  post
 });
