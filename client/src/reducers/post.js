2c5e7f89144e7165fb86ccc15e5fd69be30577f9
kevinkim1030 <kevin.kim1030@gmail.com>
2021-06-17 20:27:35 -0400

    v0 completed

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -2,7 +2,11 @@ import {
   GET_POSTS,
   POST_ERROR,
   UPDATE_LIKES,
-  DELETE_POST
+  DELETE_POST,
+  ADD_POST,
+  GET_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT
 } from '../actions/types';
 
 const initialState = {
@@ -12,7 +16,7 @@ const initialState = {
   error: {}
 }
 
-export default function(state = initialState, action){
+function postReducer(state = initialState, action){
   const { type, payload } = action;
 
   switch(type) {
@@ -22,6 +26,19 @@ export default function(state = initialState, action){
         posts: payload,
         loading: false
       };
+    case GET_POST:
+      return {
+        ...state,
+        post: payload,
+        loading: false
+      }
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [payload, ...state.posts],
+        // we want the latest post on top for UI
+        loading: false
+      }
     case DELETE_POST:
       return {
         ...state,
@@ -40,7 +57,24 @@ export default function(state = initialState, action){
         posts: state.posts.map(post => post._id === payload.id ? { ...post, likes: payload.likes} : post),
         loading: false
       }
+    case ADD_COMMENT:
+      return {
+        ...state,
+        post: {...state.post, comments: payload},
+        loading: false
+      }
+    case REMOVE_COMMENT:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          comments: state.post.comments.filter(comment => comment._id !== payload)
+        },
+        loading: false
+      }
     default: 
       return state;
   }
 }
+
+export default postReducer;
